62bf346a60e74b85fe2e29829216f07ba3934692
CarlosZiegler <carlos.ziegler@gmail.com>
2020-01-29 11:52:55 +0100

    port from Heroku

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create(
     {
-        baseURL:`https://backenddevradarapp.herokuapp.com:${process.env.PORT || 3000}`
+        baseURL:process.env.REACT_APP_API_URL
     }
 )
 
